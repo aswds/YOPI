7ec8ce53a3145bf13ed2a47331aa1d08e2808067
aswds <sasha.iilllnitskiy@gmail.com>
2022-11-09 03:05:21 +0200

    Changed task position

--- LAB1/main.py
+++ LAB1/main.py
@@ -1,7 +1,7 @@
 
 from LAB1.file_writer import writeFile
 from LAB1.Visual import showGraph
-datafile = open('../task_01_data/input_100.txt')
+datafile = open('task_01_data/input_100.txt')
 data = []
 graph = []
 
